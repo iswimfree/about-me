77a6bdb432cca22bf0987f2d10cb8bc70a185fb9
michael <mikey.m.russell@gmail.com>
2020-12-01 21:37:15 -0800

    added code

---
+++ js/app.js
@@ -0,0 +1,21 @@
+'use strict'; 
+
+
+
+// "userName" is the name of the person
+var userName = prompt('Hello my name is Mikey, welcome to my page. What is your name?');
+// console.log('the user\'s name is ' + userName)
+alert(' Welcome ' + userName);
+
+// ""
+var playGame = prompt('I sense you have time, would you like to play a game? {yes or no}');
+// console.log(user + ' said ' + playGame)
+if (playGame === 'yes' || playGame === 'y'){
+ alert('I thought you might say ' + playGame + ' lets continue');
+}
+else if(playGame < 'yes' || 'y'){
+alert('Unfortunately, no one can escape their fate. lets begin')
+}
+
+
+
